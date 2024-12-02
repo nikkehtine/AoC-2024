4ea8e1072b859aed45e060b9026efeeff32be10e
nikkehtine <nikkehtine@int.pl>
2024-12-03 00:39:07 +0100

    Write to file

--- 01/main.go
+++ 01/main.go
@@ -7,21 +7,40 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	file, err := os.Open("01/input_test")
+	in, err := os.Open("01/input_test")
 	if err != nil {
 		log.Fatalf("Error reading input file: %s", err)
 	}
-	defer file.Close()
+	defer in.Close()
 
-	s := bufio.NewScanner(file)
-	for s.Scan() {
-		fmt.Println(s.Text())
+	out, err := os.Create("01/output")
+	if err != nil {
+		log.Fatalf("Error creating output file: %s", err)
 	}
+	defer out.Close()
 
+	s := bufio.NewScanner(in)
+	w := bufio.NewWriter(out)
+	defer w.Flush()
+
+	for s.Scan() {
+		line := strings.TrimSpace(s.Text())
+		nums := strings.Fields(line)
+		numParts := len(nums)
+		if numParts != 2 {
+			log.Fatalf("Error parsing: %d entries instead of 2", numParts)
+		}
+		_, err := fmt.Fprintf(w, "%s\n", nums[0])
+		if err != nil {
+			log.Fatalf("Error writing to file: %s", err)
+		}
+	}
 	if err := s.Err(); err != nil {
 		log.Fatalf("Error reading input: %s", err)
 	}
+
 }
