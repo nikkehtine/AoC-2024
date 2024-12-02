90948364f4937b1df3123fc332287a213e055404
nikkehtine <nikkehtine@int.pl>
2024-12-02 23:54:13 +0100

    Setup

---
+++ 01/main.go
@@ -0,0 +1,13 @@
+// https://adventofcode.com/2024/day/1
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	os.Exit(0)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nikkehtine/AoC-2024
+
+go 1.23.1
