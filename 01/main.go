c0b5c639ce661fa3b39217297e2de5dc7eb3746a
nikkehtine <nikkehtine@int.pl>
2024-12-03 00:15:06 +0100

    Read input

--- 01/main.go
+++ 01/main.go
@@ -3,11 +3,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("Hello world")
-	os.Exit(0)
+	file, err := os.Open("01/input_test")
+	if err != nil {
+		log.Fatalf("Error reading input file: %s", err)
+	}
+	defer file.Close()
+
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		fmt.Println(s.Text())
+	}
+
+	if err := s.Err(); err != nil {
+		log.Fatalf("Error reading input: %s", err)
+	}
 }
